6317ee65e7addc2d75ad6d641030623666a9a39b
yakupgndz <yakupgndz@gmail.com>
2026-09-11 22:32:24 +0300

    Create Dogs.cs

---
+++ Dogs.cs
@@ -0,0 +1,26 @@
+const dogs = {
+  "801182278182": {
+    name: "Çaça",
+    page: "caca.html"
+  },
+
+  "801182278120": {
+    name: "Naga",
+    page: "naga.html"
+  },
+
+  "801182277876": {
+    name: "Boncuk",
+    page: "boncuk.html"
+  },
+
+  "801182278088": {
+    name: "Dobi",
+    page: "dobi.html"
+  },
+
+  "801182278253": {
+    name: "Muffin",
+    page: "muffin.html"
+  }
+};
